4b0e917afb7ce3293c06ab4e84cb729b3018d846
Jacob Langevei <jacob-langevei@hotmail.com>
2023-11-21 10:15:02 +0100

    Set up all routes
    Added all views and routes in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import LoginPage from '../views/LoginPage.vue';
+import WhereIsWaldoPage from '../views/WhereIsWaldoPage.vue';
+import ReactionTimePage from '../views/ReactionTimePage.vue';
+import ColorTextPage from '../views/ColorTextPage.vue';
 
 const routes = [
     {
         path: '/',
-        name: 'LoginPage',
-        component: () => import('../views/LoginPage.vue')
+        name: 'home',
+        component: LoginPage
+    },
+    {
+        path: '/where-is-waldo',
+        name: 'where-is-waldo',
+        component: WhereIsWaldoPage
+    },
+    {
+        path: '/reaction-time',
+        name: 'reaction-time',
+        component: ReactionTimePage
+    },
+    {
+        path: '/color-text',
+        name: 'color-text',
+        component: ColorTextPage
     }
 ];
 
