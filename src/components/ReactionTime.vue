72fe97efe85b62cccbc80e97e2a2afc0c0e700b1
Jacob Langevei <jacob-langevei@hotmail.com>
2023-11-21 13:53:38 +0100

    Reaction time styling fix and highscore

--- src/components/ReactionTime.vue
+++ src/components/ReactionTime.vue
@@ -1,10 +1,12 @@
 <template>
-    <section>
-        <h1>Reaction Time Game</h1>
-        <p>Click on the box below as soon as it turns green.</p>
-        <div id="colorBlock" class="color-block" :class="{ green: isGreen, red: !isGreen }" @click="checkReactionTime"></div>
+    <section class="full-screen" :class="{ green: isGreen, red: !isGreen }" @click="checkReactionTime">
+        <div v-if="!isGreen" class="message">Wait for green</div>
+        <div v-else class="message">Click!</div>
+        <div v-if="endTime" class="reaction-time">Your reaction time: {{ reactionTime }} ms</div>
+        <div class="high-score" v-if="highScore">High Score: {{ highScore }} ms</div>
     </section>
 </template>
+
 
 <script>
 export default {
@@ -13,38 +15,57 @@ export default {
       startTime: null,
       endTime: null,
       isGreen: false,
+      reactionTime: null,
+      highScore: this.getHighScore(), // Initialize high score from localStorage
     };
   },
   methods: {
     checkReactionTime() {
       if (this.isGreen) {
         this.endTime = new Date().getTime();
-        let reactionTime = this.endTime - this.startTime;
-        alert(`Your reaction time is ${reactionTime} milliseconds.`);
+        this.reactionTime = this.endTime - this.startTime;
         this.isGreen = false;
-        this.changeColor();
+        this.updateHighScore(this.reactionTime);
+        setTimeout(this.changeColor, 2000); // Wait 2 seconds before starting over
       }
     },
     changeColor() {
+      this.endTime = null; // Reset the end time
+      this.reactionTime = null; // Reset the reaction time
       setTimeout(() => {
         this.isGreen = true;
         this.startTime = new Date().getTime();
-      }, Math.floor(Math.random() * 5000) + 1000); // Changes color between 1 to 5 seconds randomly
+      }, Math.floor(Math.random() * 5000) + 1000); // Change color randomly between 1 to 5 seconds
     },
+    getHighScore() {
+      return localStorage.getItem('highScore') || null;
+    },
+    updateHighScore(newTime) {
+      if (this.highScore === null || newTime < this.highScore) {
+        this.highScore = newTime;
+        localStorage.setItem('highScore', newTime);
+      }
+    }
   },
   mounted() {
     this.changeColor();
   },
 };
-
 </script>
 
+
 <style>
-    .color-block {
-  width: 200px;
-  height: 200px;
-  margin: 20px auto;
+.full-screen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
   transition: background-color 0.3s ease;
+  cursor: pointer;
+  font-family: 'Arial', sans-serif;
+  user-select: none; /* Prevent user selection on double clicks */
 }
 
 .red {
@@ -53,6 +74,18 @@ export default {
 
 .green {
   background-color: green;
+}
+
+.message {
+  font-size: 2rem;
+  color: white;
+  text-align: center;
+  margin: 20px;
+}
+
+.reaction-time {
+  color: white;
+  font-size: 1.5rem;
 }
 
 </style>
